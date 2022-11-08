26ca0b8c4fe5d4fdfaccb26f2723527b15783333
Minjur <minjur.rahaman99@gmail.com>
2022-11-08 16:22:57 +0600

    Added second another section in home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './About/About';
+import ChosesUs from './ChosesUS/ChosesUs';
 import Slider from './Slider/Slider';
 
 const Home = () => {
@@ -7,6 +8,8 @@ const Home = () => {
         <div>
             <Slider></Slider>
             <About></About>
+            <ChosesUs></ChosesUs>
+
         </div>
     );
 };
