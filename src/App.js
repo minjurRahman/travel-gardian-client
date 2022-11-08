52d7951d69614f702832f570ca9b6fb1dab87129
Minjur <minjur.rahaman99@gmail.com>
2022-11-08 11:41:50 +0600

    main layout added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/Routes';
 
 function App() {
+
+  
   return (
     <div className="">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
